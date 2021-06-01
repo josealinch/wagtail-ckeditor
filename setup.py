6f8ef5eeb6b112b3d21d08afd38758b6c7e05829
ghettoDdOS <47575319+ghettoDdOS@users.noreply.github.com>
2021-06-01 14:32:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="wagtail-ckeditor",
-    version="1.0",
+    version="1.1",
     description="CKEditor widget for Wagtail CMS",
     author="ghettoDdOS",
     author_email="pen.egor2002@gmail.com",
