fa9bbb02a46d838ec6c5e89cae62c06ec0afeb20
Manay Kimak <62600032+ayman-makki@users.noreply.github.com>
2023-07-17 09:17:55 +0200

    Update wagtail_hooks.py

--- wagtail_ckeditor/wagtail_hooks.py
+++ wagtail_ckeditor/wagtail_hooks.py
@@ -1,6 +1,6 @@
 from wagtail.admin.rich_text.converters.editor_html import WhitelistRule
-from wagtail.core import hooks
-from wagtail.core.whitelist import allow_without_attributes, attribute_rule
+from wagtail import hooks
+from wagtail.whitelist import allow_without_attributes, attribute_rule
 
 
 @hooks.register("register_rich_text_features")
